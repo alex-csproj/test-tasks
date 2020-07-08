0393dfa3bbbc0347ff156744e0e55e99e95c181e
Alex <boxy-poxy@yandex.ru>
2020-07-08 23:31:55 +0300

    Added 'SimpleRobot'

---
+++ Robots/TestTasks.Robots.Tests/SimpleRobotTests.cs
@@ -0,0 +1,178 @@
+﻿using FluentAssertions;
+using System;
+using TestTasks.Robots.Contracts;
+using Xunit;
+
+namespace TestTasks.Robots.Tests
+{
+    public class SimpleRobotTests
+    {
+        private static readonly Orientation defaultOrientation = Orientation.N;
+        private static readonly Position defaultPosition = Position.Origin;
+        private static readonly Position innerPosition = new Position(2, 2);
+        private static readonly Position outerPosition = new Position(3, 3);
+
+        private readonly IArea defaultArea = new RectangularArea(new Position(-2, -2), new Position(2, 2));
+
+        private SimpleRobot CreateSut() =>
+            new SimpleRobot() { Area = defaultArea, LastPosition = defaultPosition, Orientation = defaultOrientation };
+
+        [Fact]
+        public void SetLastPosition_WithArea_DoesNotThrow()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Action act = () => sut.LastPosition = defaultPosition;
+
+            // Assert
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void SetLastPosition_WithoutArea_Throws()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+            sut.Area = null;
+
+            // Act
+            Action act = () => sut.LastPosition = defaultPosition;
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage($"Can not set position without area.");
+        }
+
+        [Fact]
+        public void Status_ByDefault_IsNormal()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Assert
+            sut.Status.Should().Be(Status.Normal);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfDoesNotMoveOverEdge_ChangesLastPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Position start = sut.LastPosition;
+            sut.LastPosition = innerPosition;
+
+            // Assert
+            sut.LastPosition.Should().Be(innerPosition);
+            sut.LastPosition.Should().NotBe(start);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfMovesOverEdge_DoesNotChangeLastPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Position start = sut.LastPosition;
+            sut.LastPosition = outerPosition;
+
+            // Assert
+            sut.LastPosition.Should().Be(start);
+            sut.LastPosition.Should().NotBe(outerPosition);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfMovesOverEdge_SetsStatusToLost()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            sut.LastPosition = outerPosition;
+
+            // Assert
+            sut.Status.Should().Be(Status.Lost);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfDoesNotMoveOverEdge_DoesNotAddScentPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            sut.LastPosition = innerPosition;
+
+            // Assert
+            sut.Area.ScentPosition.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void SetLastPosition_IfMovesOverEdge_AddsScentPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            sut.LastPosition = outerPosition;
+
+            // Assert
+            sut.Area.ScentPosition.Should().Equal(outerPosition);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfLost_DoesNotChangeLastPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Position start = sut.LastPosition;
+            sut.LastPosition = outerPosition;
+            sut.LastPosition = innerPosition;
+
+            // Assert
+            sut.LastPosition.Should().Be(start);
+            sut.LastPosition.Should().NotBe(innerPosition);
+        }
+
+        [Fact]
+        public void SetLastPosition_IfMovesToScentPosition_IgnoresMove()
+        {
+            // Arrange
+            SimpleRobot robot = CreateSut();
+            robot.LastPosition = outerPosition;
+
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Position start = sut.LastPosition;
+            sut.LastPosition = outerPosition;
+            sut.LastPosition = innerPosition;
+
+            // Assert
+            sut.LastPosition.Should().Be(innerPosition);
+            sut.LastPosition.Should().NotBe(start);
+        }
+
+        [Fact]
+        public void _SetLastPosition_IfLost_DoesNotChangeLastPosition()
+        {
+            // Arrange
+            SimpleRobot sut = CreateSut();
+
+            // Act
+            Orientation start = sut.Orientation;
+            sut.LastPosition = outerPosition;
+            sut.Orientation = Orientation.E;
+
+            // Assert
+            sut.Orientation.Should().Be(start);
+            sut.Orientation.Should().NotBe(Orientation.E);
+        }
+    }
+}
---
+++ Robots/TestTasks.Robots/SimpleRobot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TestTasks.Robots.Contracts;
+
+namespace TestTasks.Robots
+{
+    public class SimpleRobot : IRobot
+    {
+        private Orientation orientation = Orientation.N;
+        private Position lastPosition = Position.Origin;
+
+        public IArea Area { get; set; }
+
+        public Orientation Orientation
+        {
+            get => orientation;
+            set
+            {
+                if (Status == Status.Lost)
+                    return;
+
+                orientation = value;
+            }
+        }
+
+        public Position LastPosition
+        {
+            get => lastPosition;
+            set
+            {
+                if (Area == null)
+                    throw new InvalidOperationException("Can not set position without area.");
+
+                if (Status == Status.Lost)
+                    return;
+
+                if (Area.ScentPosition.Contains(value))
+                    return;
+
+                if (Area.Contains(value))
+                {
+                    lastPosition = value;
+                }
+                else
+                {
+                    Status = Status.Lost;
+                    Area.ScentPosition.Add(value);
+                }
+            }
+        }
+
+        public Status Status { get; private set; } = Status.Normal;
+
+        public override string ToString() =>
+            $"{{Position: {LastPosition}; Orientation: {Orientation}; Status: {Status}}}";
+    }
+}
