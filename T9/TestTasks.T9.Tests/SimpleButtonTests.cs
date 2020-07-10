16b68718770de02a4bf72fb00ca5e5fd1aef957c
Alex <boxy-poxy@yandex.ru>
2020-07-10 22:09:00 +0300

    Added 'SimpleButton'

---
+++ T9/TestTasks.T9.Tests/SimpleButtonTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace TestTasks.T9.Tests
+{
+    public class SimpleButtonTests
+    {
+        [Fact]
+        public void Ctor_IfSymbolsNull_Throws()
+        {
+            // Act
+            Action act = () => new SimpleButton('*', null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>()
+                .WithMessage("Value cannot be null. (Parameter 'symbols')");
+        }
+
+        [Fact]
+        public void Ctor_IfSymbolsNotNull_DoesNotThrow()
+        {
+            // Act
+            Action act = () => new SimpleButton('*', new char[0]);
+
+            // Assert
+            act.Should().NotThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Ctor_IfSymbolsContainDuplicates_Throws()
+        {
+            // Act
+            Action act = () => new SimpleButton('*', "aa");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Duplicates are not allowed in 'symbols'.");
+        }
+
+        [Fact]
+        public void Ctor_IfSymbolsDoNotContainDuplicates_DoesNotThrow()
+        {
+            // Act
+            Action act = () => new SimpleButton('*', "ab");
+
+            // Assert
+            act.Should().NotThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void Ctor_SetsProperties()
+        {
+            // Act
+            SimpleButton sut = new SimpleButton('*', "ab");
+
+            // Assert
+            sut.Label.Should().Be('*');
+            sut.Symbols.Should().Equal(new[] { 'a', 'b' });
+        }
+    }
+}
---
+++ T9/TestTasks.T9/SimpleButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestTasks.T9.Contract;
+
+namespace TestTasks.T9
+{
+    public class SimpleButton : IButton
+    {
+        public char Label { get; }
+
+        public IReadOnlyList<char> Symbols { get; }
+
+        public SimpleButton(char label, IEnumerable<char> symbols)
+        {
+            if (symbols == null)
+                throw new ArgumentNullException(nameof(symbols));
+
+            Symbols = symbols.ToArray();
+
+            if (Symbols.Distinct().Count() != Symbols.Count)
+                throw new ArgumentException($"Duplicates are not allowed in '{nameof(symbols)}'.");
+
+            Label = label;
+        }
+    }
+}
