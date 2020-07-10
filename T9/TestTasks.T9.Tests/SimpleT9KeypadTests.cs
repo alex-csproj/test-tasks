9b54c7028e70e888219b4cdc53b8951a9e450a0f
Alex <boxy-poxy@yandex.ru>
2020-07-10 23:06:26 +0300

    Added 'SimpleT9Keypad'

---
+++ T9/TestTasks.T9.Tests/SimpleT9KeypadTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using System;
+using TestTasks.T9.Contract;
+using Xunit;
+
+namespace TestTasks.T9.Tests
+{
+    public class SimpleT9KeypadTests
+    {
+        [Fact]
+        public void GetButton_IfButtonDoesNotExist_Throws()
+        {
+            // Arrange
+            SimpleT9Keypad sut = new SimpleT9Keypad();
+
+            // Act
+            Action act = () => sut.GetButton('*');
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Button with given symbol '*' does not exist.");
+        }
+
+        [Fact]
+        public void GetButton_IfButtonExist_DoesNotThrow()
+        {
+            // Arrange
+            SimpleT9Keypad sut = new SimpleT9Keypad();
+
+            // Act
+            Action act = () => sut.GetButton('e');
+
+            // Assert
+            act.Should().NotThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void GetButton_ReturnsExpectedButton()
+        {
+            // Arrange
+            SimpleT9Keypad sut = new SimpleT9Keypad();
+
+            // Act
+            IT9Button button = sut.GetButton('e');
+
+            // Assert
+            button.Label.Should().Be('3');
+        }
+
+        [Fact]
+        public void TryGetButton_IfButtonDoesNotExist_ReturnsFalse()
+        {
+            // Arrange
+            SimpleT9Keypad sut = new SimpleT9Keypad();
+
+            // Act
+            bool exists = sut.TryGetButton('*', out IT9Button button);
+
+            // Assert
+            exists.Should().BeFalse();
+            button.Should().BeNull();
+        }
+
+        [Fact]
+        public void TryGetButton_IfButtonExist_ReturnsTrue()
+        {
+            // Arrange
+            SimpleT9Keypad sut = new SimpleT9Keypad();
+
+            // Act
+            bool exists = sut.TryGetButton('e', out IT9Button button);
+
+            // Assert
+            exists.Should().BeTrue();
+            button.Label.Should().Be('3');
+        }
+    }
+}
---
+++ T9/TestTasks.T9/SimpleT9Keypad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestTasks.T9.Contract;
+
+namespace TestTasks.T9
+{
+    public class SimpleT9Keypad : IT9Keypad
+    {
+        private static readonly IReadOnlyList<IT9Button> buttons =
+            new[]
+            {
+                new SimpleButton('2',"abc"),
+                new SimpleButton('3',"def"),
+                new SimpleButton('4',"ghi"),
+                new SimpleButton('5',"jkl"),
+                new SimpleButton('6',"mno"),
+                new SimpleButton('7',"pqrs"),
+                new SimpleButton('8',"tuv"),
+                new SimpleButton('9',"wxyz"),
+                new SimpleButton('0'," ")
+            }
+            .Select(button => new SimpleT9Button(button))
+            .ToArray();
+
+        private static readonly IReadOnlyDictionary<char, IT9Button> buttonBySymbol =
+            buttons
+            .SelectMany(button => button.Symbols.Select(symbol => (symbol, button)))
+            .ToDictionary(i => i.symbol, i => i.button);
+
+        public IT9Button GetButton(char symbol)
+        {
+            try
+            {
+                return buttonBySymbol[symbol];
+            }
+            catch(KeyNotFoundException)
+            {
+                throw new ArgumentException($"Button with given symbol '{symbol}' does not exist.");
+            }
+        }
+
+        public bool TryGetButton(char symbol, out IT9Button button) =>
+            buttonBySymbol.TryGetValue(symbol, out button);
+    }
+}
