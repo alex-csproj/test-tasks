a5c1762b931cf14b827d7d3ddd2b15a33e563100
Alex <boxy-poxy@yandex.ru>
2020-07-11 00:16:44 +0300

    Created demo application.

--- T9/TestTasks.T9.Demo/Program.cs
+++ T9/TestTasks.T9.Demo/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace TestTasks.T9.Demo
+﻿using System;
+using System.Linq;
+
+namespace TestTasks.T9.Demo
 {
     class Program
     {
@@ -9,7 +12,14 @@
 
         private void Run()
         {
+            var count = int.Parse(Console.ReadLine());
+            var inputs = Enumerable.Range(0, count)
+                                   .Select(i => Console.ReadLine())
+                                   .ToArray();
 
+            var converter = new SimpleTextToT9Converter(new SimpleT9Keypad());
+            for (var i = 0; i < inputs.Length; i++)
+                Console.WriteLine($"Case #{i + 1}: {converter.Convert(inputs[i])}");
         }
     }
 }
