d2d985c1b9f4453c5a5c71e58859d23a44a7276c
Alex <boxy-poxy@yandex.ru>
2020-07-10 18:52:23 +0300

    Simplified and optimized 'FileDownloadManager'.

--- AsyncService/TestTasks.FileDownloader/FileDownloadManager.cs
+++ AsyncService/TestTasks.FileDownloader/FileDownloadManager.cs
@@ -7,8 +7,8 @@ namespace TestTasks.FileDownloader
 {
     public class FileDownloadManager : IFileDownloader
     {
-        private readonly IDictionary<string, DownloadHelper> cache =
-            new ConcurrentDictionary<string, DownloadHelper>();
+        private readonly IDictionary<string, Task<FileResult>> cache =
+            new ConcurrentDictionary<string, Task<FileResult>>();
 
         private readonly IFileDownloader downloader;
 
@@ -18,49 +18,14 @@ namespace TestTasks.FileDownloader
         public FileResult Download(string file) =>
             DownloadAsync(file).Result;
 
-        public async Task<FileResult> DownloadAsync(string file)
+        public Task<FileResult> DownloadAsync(string file)
         {
-            if (cache.TryGetValue(file, out var helper))
-                helper.AddClient();
-            else
-                helper = (cache[file] = new DownloadHelper(downloader.DownloadAsync(file)));
+            if (cache.TryGetValue(file, out var result))
+                return result;
 
-            var result = await helper.FileResult;
-
-            helper.RemoveClient();
-            if (!helper.HasClients())
-                cache.Remove(file);
-
+            result = (cache[file] = downloader.DownloadAsync(file));
+            result.ContinueWith(_ => cache.Remove(file));
             return result;
-        }
-
-        private class DownloadHelper
-        {
-            private int clients = 1;
-            private readonly object sync = new object();
-
-            public Task<FileResult> FileResult { get; }
-
-            public DownloadHelper(Task<FileResult> fileResult) =>
-                FileResult = fileResult;
-
-            public void AddClient()
-            {
-                lock (sync)
-                    clients++;
-            }
-
-            public bool HasClients()
-            {
-                lock (sync)
-                    return clients > 0;
-            }
-
-            public void RemoveClient()
-            {
-                lock (sync)
-                    clients--;
-            }
         }
     }
 }
